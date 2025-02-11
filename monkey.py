3f38b817ba73869eeeed52c568aed1f2ec39961b
KatlehoMoeketsi <kaydawg718@gmail.com>
2025-02-11 19:06:50 +0200

    Added assets folder with fonts and applied to the two main labels

--- monkey.py
+++ monkey.py
@@ -1,3 +1,4 @@
+from docutils.nodes import title
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
@@ -5,6 +6,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivymd.theming import ThemeManager
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 import requests
@@ -12,19 +14,37 @@ import sqlite3
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivy.core.window import Window
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDIconButton
 from kivy.core.text import  LabelBase
 import random
+
+from kivymd.uix.textfield import MDTextField
+
+class AddWordContent(MDBoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        self.word_input  = MDTextField(hint_text="Enter a word")
+        self.size_hint_y = None
+        self.add_widget(self.word_input)
+
+
 
 class Monkey(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.content = None
+        self.word_input = None
+        self.dialog = None
+        self.screen = None
+        self.about_program = None
+        self.add_word_btn = None
+        self.left_section = None
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.accent_palette = "Amber"
         self.theme_cls.primary_hue = "500"
-        # Window.fullscreen = 'auto'
-        LabelBase.register(name="Cakeroll", fn_regular="assets/Cakeroll.ttf")
-        LabelBase.register(name="SquareKids", fn_regular="assets/SquareKids.ttf")
         self.info = None
         self.bottom = None
         self.generate_word = None
@@ -33,11 +53,39 @@ class Monkey(MDApp):
         self.header_label = None
         self.header = None
         self.layout = None
+
+        # Window.fullscreen = 'auto'
+        Window.clear_color = (0.565, 0.933, 0.565, 1)
+        LabelBase.register(name="TeachersPet", fn_regular="assets/teachersPet.ttf")
+        LabelBase.register(name="SquareKids", fn_regular="assets/SquareKids.ttf")
         self.conn= sqlite3.connect("monkeywords.db")
         self.init_db()
 
     def build(self):
-        self.layout = BoxLayout(orientation="vertical", padding=5, spacing=10)
+        self.screen = MDScreen(md_bg_color =(0.565, 0.933, 0.565, 1))
+
+        #Main Layout is root which will contain two boxes side by side
+        self.root = BoxLayout(orientation="horizontal")
+
+        #first box situated on the right, and it is the main layout carrying most elements
+        self.layout = BoxLayout(orientation="vertical")
+
+        #second box situated on the left, and contains the add action and about window
+        self.left_section = BoxLayout(
+            orientation="vertical",
+            size_hint_x=0.1,
+            )
+        #ImageButtons on the left comprising the menu of the program.
+        # It will disappear once we go full screen.
+        self.add_word_btn = MDIconButton(icon= "assets/images/add.png",icon_size="50sp", on_release=lambda x:print("Add Button Clicked"))
+        self.about_program = MDIconButton(icon= "assets/images/about.png",icon_size="50sp", on_release=lambda x:print("About Button clicked"))
+        self.about_program.bind(on_press=self.show_about_dialog)
+        self.add_word_btn.bind(on_press=self.add_word_dialog)
+
+        self.left_section.add_widget(self.add_word_btn)
+        self.left_section.add_widget(self.about_program)
+
+
 
         #Top section
         self.header = BoxLayout(
@@ -72,7 +120,8 @@ class Monkey(MDApp):
                                     text_size=(400, None),
                                     halign="center",
                                     valign="middle",
-                                    font_name="Cakeroll")
+                                    font_name="TeachersPet")
+
         self.generate_word = Button(text="Generate", size_hint=(1,None), height=30)
         self.generate_word.bind(on_press=self.generate_random_word)
 
@@ -93,7 +142,9 @@ class Monkey(MDApp):
         self.bottom.add_widget(self.info)
         self.layout.add_widget(self.bottom)
 
-        return self.layout
+        self.root.add_widget(self.left_section)
+        self.root.add_widget(self.layout)
+        return self.root
 
     def init_db(self):
         cursor = self.conn.cursor()
@@ -118,3 +169,31 @@ class Monkey(MDApp):
         else:
             self.generate_label.text="No words found"  # Handle empty database
 
+    def add_word_dialog(self, instance):
+        if not self.dialog:
+            self.content = AddWordContent()
+            self.dialog = MDDialog(
+                title = "Add a word",
+                type="custom",
+
+                height = "200dp",
+                pos_hint={"center_y":0.5},
+                content_cls= self.content,
+                buttons = [
+                    MDFlatButton(text = "Cancel", on_release=lambda x: self.dialog.dismiss()),
+                    MDFlatButton(text = "Add", on_release=lambda x: self.add_words),
+            ],
+        )
+        self.dialog.open()
+
+    def add_words(self,instance):
+        pass
+
+    def show_about_dialog(self, instance):
+        dialog = MDDialog(
+            title = "About this App",
+            text= "Sample application using KivyMD.\nBuilt with Python and Kivy",
+            buttons=[MDFlatButton(text="Close", on_release=lambda x:dialog.dismiss())
+                ],
+        )
+        dialog.open()
